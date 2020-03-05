d442524f5a0092c5864bebf0a7d1844b6549a6ca
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-05 20:34:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,31 @@ end
 def pop_with_args(array)
   array.pop(2)
 end  
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array1, array2)
+  
+end
+
+def using_insert(array, element)
+  
+end
+
+def using_flatten(array)
+  
+end
+
+def using_delete(array, string)
+  
+end
+
+def using_delete_at (array, integer)
+  
+end 
